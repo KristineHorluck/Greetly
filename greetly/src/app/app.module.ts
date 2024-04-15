6d61f343887d55b3bb0a592049e21fd7e505aa48
Kristine Hørlück <kristine.horluck@outlook.com>
2024-04-15 10:04:07 +0200

    Remove unused dependencies and update .gitignore

---
+++ greetly/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from '../../../greetly/src/app/app-routing.module';
+import { AppComponent } from '../../../greetly/src/app/app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
