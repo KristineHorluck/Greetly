421466688e5921aaee15f11f54955457c8722b41
Diem-Vi <diemvinguyen2004@gmail.com>
2024-04-30 20:35:07 +0200

    Half assed left-panel
    Fuck mit liv

--- greetly/src/app/explore-page/panels/panels.component.ts
+++ greetly/src/app/explore-page/panels/panels.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon'; // added for button
+import {MatDividerModule} from '@angular/material/divider'; // added for button
+import {MatButtonModule} from '@angular/material/button'; // added for button
 
 @Component({
   selector: 'app-panels',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule], // added the import for the button
   templateUrl: './panels.component.html',
   styleUrl: './panels.component.css'
 })
 export class PanelsComponent {
   term: any;
 }
+
