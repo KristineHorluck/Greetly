bbfe9a3f467cd95bb5954e4b55d56425bc45eb16
Diem-Vi <diemvinguyen2004@gmail.com>
2024-05-13 14:26:56 +0200

    explore page
    Basically done

---
+++ greetly/src/app/explore-page/panels/panel-left/opret-begivenhed/opret-begivenhed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opret-begivenhed',
+  standalone: true,
+  imports: [],
+  templateUrl: './opret-begivenhed.component.html',
+  styleUrl: './opret-begivenhed.component.css'
+})
+export class OpretBegivenhedComponent {
+
+}
