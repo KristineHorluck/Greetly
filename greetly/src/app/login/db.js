20bcbfd3dbca79bc014a0a2f93d6e37643d8eecd
iandre23 <idaandreasen66@gmail.com>
2024-05-24 10:35:22 +0200

    nu virker det. skal bare ryddes op i

---
+++ greetly/src/app/login/db.js
@@ -0,0 +1,14 @@
+// Code that connects to mySQL and query to the database
+/*import mysql from 'mysql2'
+
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'brugernavn',
+    password: 'kode', // Din egen adgangskode (til serveren)
+    database: 'login_info' // Ændre hvis din database hedder noget andet
+}).promise()
+
+
+const result = await pool.query("SELECT * FROM login") // Min table hedder login, ændrer hvis du har kaldt den noget andet.
+console.log(result)
