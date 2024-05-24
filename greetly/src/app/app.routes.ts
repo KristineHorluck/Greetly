871fd0174b015bfc1959e228bc1048e765f509ef
Diem-Vi <diemvinguyen2004@gmail.com>
2024-05-24 11:52:25 +0200

    Fikset profile page

--- greetly/src/app/app.routes.ts
+++ greetly/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { ChatComponent } from './Blackbox/chat/chat.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { FavouritesComponent } from './favourites/favourites.component';
-import { ExplorePageComponent } from './explore-page/explore-page.component';
 import { AboutUsComponent } from './Blackbox/about-us/about-us.component';
 import { ContactComponent } from './Blackbox/contact/contact.component';
 import { FAQComponent } from './Blackbox/faq/faq.component';
 import { PoliticsComponent } from './Blackbox/politics/politics.component';
 import { RulesComponent } from './Blackbox/rules/rules.component';
+import { PanelsComponent } from './explore-page/panels/panels.component';
 
 
 
@@ -15,10 +15,11 @@ export const routes: Routes = [
     {'path': 'chat', component:ChatComponent},
     {'path': 'profile', component:ProfilePageComponent},
     {'path': 'favourites', component:FavouritesComponent},
-    {'path': '', component:ExplorePageComponent},
     {'path': 'about us', component:AboutUsComponent},
     {'path': 'contact', component:ContactComponent},
     {'path': 'FAQ', component:FAQComponent},
     {'path': 'politics', component:PoliticsComponent},
     {'path': 'rules', component:RulesComponent},  
+    {'path': 'panels', component:PanelsComponent},
+    {'path': '**', component:PanelsComponent},
 ];
